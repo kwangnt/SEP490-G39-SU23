f418ee518b245d3816f4ec982f0929c09c1f49d9
Thanh <thanhnvhe140127@fpt.edu.vn>
2023-05-22 23:28:53 +0700

    Update

---
+++ TeachSync/src/main/java/com/teachsync/TeachSync/TeachSyncApplication.java
@@ -0,0 +1,13 @@
+package com.teachsync.TeachSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachSyncApplication.class, args);
+	}
+
+}
