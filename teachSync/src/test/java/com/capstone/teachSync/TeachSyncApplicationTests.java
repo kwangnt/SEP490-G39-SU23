fc04a9d972f2f1efcb4b29b970e408166e968dee
ThanhNMSE130286 <61862196+ThanhNMSE130286@users.noreply.github.com>
2023-05-22 15:36:21 +0700

    Add file via upload

---
+++ teachSync/src/test/java/com/capstone/teachSync/TeachSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.teachSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
