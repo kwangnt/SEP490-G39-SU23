204127494929fc36b84998ec1a545274d460f25f
Do Viet Hung <133358028+hungdv132@users.noreply.github.com>
2023-06-06 23:03:28 +0700

    test

--- TeachSync/src/main/java/com/teachsync/TeachSync/role.java
+++ TeachSync/src/main/java/com/teachsync/TeachSync/role.java
@@ -2,4 +2,10 @@ package com.teachsync.TeachSync;
 
 import jakarta.persistence.Entity;
 
-
+@RestController
+public class huhu {
+    @GetMapping("tt")
+    String demi(){
+        return "huhu";
+    }
+}
