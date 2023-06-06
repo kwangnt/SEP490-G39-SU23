4e60476b717517f22bfe8eb44766ca2eb25cf02e
Thanh <thanhnvhe140127@fpt.edu.vn>
2023-06-07 00:35:09 +0700

    update list child

--- TeachSync/src/main/java/com/teachsync/controllers/ChildController.java
+++ TeachSync/src/main/java/com/teachsync/controllers/ChildController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,15 +20,21 @@ public class ChildController {
 
     @GetMapping("/listchild")
     public String lstChild(Model model) {
-            System.out.println("skdfn");
-            List<User> lst = userService.getListUserByType(2L);
-            model.addAttribute("lstUser", lst);
-            if (lst.isEmpty()) {
-                System.out.println("khong tim thay du lieu");
-            } else {
-                System.out.println(lst);
-            }
-            return "list-user";
+        System.out.println("skdfn");
+        List<User> lst = userService.getListUserByType(2L);
+        model.addAttribute("lstUser", lst);
+        if (lst.isEmpty()) {
+            System.out.println("khong tim thay du lieu");
+        } else {
+            System.out.println(lst);
+        }
+        return "list-user";
+    }
+
+    @GetMapping("/searchuserbyusername")
+    public String searchByUserName(@RequestParam String searchText, Model model) {
+        model.addAttribute("lstUser", userService.getListUserByUserName(searchText));
+        return "list-user";
     }
 
 }
