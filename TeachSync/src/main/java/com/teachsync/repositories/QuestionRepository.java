74326c3e5669011d56d06af03975f1691cee0d81
Thanh <thanhnvhe140127@fpt.edu.vn>
2023-06-27 22:44:22 +0700

    update

--- TeachSync/src/main/java/com/teachsync/repositories/QuestionRepository.java
+++ TeachSync/src/main/java/com/teachsync/repositories/QuestionRepository.java
@@ -4,6 +4,10 @@ package com.teachsync.repositories;
 import com.teachsync.entities.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends
         JpaRepository<Question, Long> {
+
+    List<Question> findAllByIdTest(Long idTest);
 }
