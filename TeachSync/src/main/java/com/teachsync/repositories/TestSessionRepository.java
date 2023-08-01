20f065460810a391c631989502a3e83d6f33e755
Thanh <thanhnvhe140127@fpt.edu.vn>
2023-08-01 19:08:19 +0700

    update thời gian test

---
+++ TeachSync/src/main/java/com/teachsync/repositories/TestSessionRepository.java
@@ -0,0 +1,10 @@
+package com.teachsync.repositories;
+
+import com.teachsync.entities.TestSession;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestSessionRepository extends JpaRepository<TestSession, Long> {
+    Page<TestSession> findAllByOrderByStartDateDesc(Pageable pageable);
+}
